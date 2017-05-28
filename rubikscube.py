cafb5e1a101adc05a2dee6a88a2676c6b1c6bafa
bdinh <baodinh96@gmail.com>
2017-05-28 15:24:49 -0700

    added operators close #3

--- rubikscube.py
+++ rubikscube.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 class Cube:
 
@@ -19,6 +20,10 @@ class Cube:
         """
         self.N = 2
         self.flatBoard = np.array([np.tile(i, (self.N, self.N)) for i in range(6)])
+        self.npWinningState = self.flatBoard
+        self.randomize(10)
+
+
 
     def randomize(self, n):
         """
@@ -28,10 +33,21 @@ class Cube:
             f = self.dictface[np.random.randint(6)]
             l = np.random.randint(self.N)
             d = 1 + np.random.randint(3)
-            self.move(f, l, d)
+            self.moveCube(f, l, d)
         return None
 
-    def move(self, f, l, d):
+    def turn(self, f, d):
+        """
+        Turn whole cube (without making a layer move) around face `f`
+        `d` 90-degree turns in the clockwise direction.  Use `d=3` or
+        `d=-1` for counter-clockwise.
+        """
+        for l in range(self.N):
+            self.moveCube(f, l, d)
+        return None
+
+
+    def moveCube(self, f, l, d):
         """
         Make a layer move of layer `l` parallel to face `f` through
         `d` 90-degree turns in the clockwise direction.  Layer `0` is
@@ -52,7 +68,7 @@ class Cube:
                               (self.facedict["B"], range(self.N), l2),
                               (self.facedict["L"], range(self.N), l2)])
         if f == "D":
-            return self.move("U", l2, -d)
+            return self.moveCube("U", l2, -d)
         if f == "F":
             f2 = "B"
             i2 = self.facedict[f2]
@@ -62,7 +78,7 @@ class Cube:
                               (self.facedict["D"], range(self.N)[::-1], l2),
                               (self.facedict["R"], l, range(self.N)[::-1])])
         if f == "B":
-            return self.move("F", l2, -d)
+            return self.moveCube("F", l2, -d)
         if f == "R":
             f2 = "L"
             i2 = self.facedict[f2]
@@ -72,7 +88,7 @@ class Cube:
                               (self.facedict["D"], l2, range(self.N)),
                               (self.facedict["B"], l, range(self.N)[::-1])])
         if f == "L":
-            return self.move("R", l2, -d)
+            return self.moveCube("R", l2, -d)
         for d in ds:
             if l == 0:
                 self.flatBoard[i] = np.rot90(self.flatBoard[i], 3)
@@ -95,9 +111,148 @@ class Cube:
         return None
 
 
+    def getCubeState(self):
+        self.result = []
+        for i in range(len(self.flatBoard)):
+            add = []
+            add.append(self.flatBoard[i].tolist())
+            self.result.append(add)
+
+    #
+    # def __str__(self):
+    #     result = []
+    #     for i in range(len(self.flatBoard)):
+    #         result.append(self.flatBoard[i])
+    #     return result
+
+
+class Operator:
+
+    def __init__(self, name, precond, state_transf):
+        self.name = name
+        self.precond = precond
+        self.state_trasf = state_transf
+
+    def is_applicable(self, s):
+        return self.precond(s)
+
+    def apply(self, s):
+        return self.state_trasf(s)
+
+
+def can_move(s):
+    'Always true because no restriction on when to move'
+    return True
+
+def move(s, f, l, d):
+    s.moveCube(f, l, d)
+    return s.flatBoard
+
+
+# Twelve operators for each face. clock wise (cw) and counter clock wise (ccw) turns
+UpCWOP = Operator("Up Face Clock Wise Turn",
+                    lambda s: can_move(s),
+                    lambda s: move(s, "U", 0, 1))
+
+UpCCWOP = Operator("Up Face Counter Close Wise Turn",
+                    lambda s: can_move(s),
+                    lambda s: move(s, "U", 0, -1))
+
+DownCWOP = Operator("Down Face Clock Wise Turn",
+                    lambda s: can_move(s),
+                    lambda s: move(s, "D", 0, 1))
+
+DownCCWOP = Operator("Down Face Counter Clock Turn",
+                    lambda s: can_move(s),
+                    lambda s: move(s, "D", 0, -1))
+
+FrontCWOP = Operator("Front Face Clockwise Turn",
+                    lambda s: can_move(s),
+                    lambda s: move(s, "F", 0, 1))
+
+FrontCCWOP = Operator("Front Face Counter Clockwise Turn",
+                    lambda s: can_move(s),
+                    lambda s: move(s, "F", 0, -1))
+
+BackCWOP = Operator("Back Face Clockwise Turn",
+                    lambda s: can_move(s),
+                    lambda s: move(s, "B", 0, 1))
+
+BackCCWOP = Operator("Back Face Counter Clockwise Turn",
+                    lambda s: can_move(s),
+                    lambda s: move(s, "B", 0, -1))
+
+LeftCWOP = Operator("Left Face Clock Wise Turn",
+                    lambda s: can_move(s),
+                    lambda s: move(s, "L", 0, 1))
+
+LeftCCWOP = Operator("Left Face Counter Clockwise Turn",
+                    lambda s: can_move(s),
+                    lambda s: move(s, "L", 0, -1))
+
+RightCWOP = Operator("Right Face Clockwise Turn",
+                     lambda s: can_move(s),
+                     lambda s: move(s, "R", 0, 1))
+
+
+RightCCWOP = Operator("Right Face Counter Clockwise Turn",
+                     lambda s: can_move(s),
+                     lambda s: move(s, "R", 0, -1))
+
+
+OPERATORS = [UpCWOP, UpCCWOP, DownCWOP, DownCCWOP,
+             FrontCWOP, FrontCCWOP, BackCWOP, BackCCWOP,
+             LeftCWOP, LeftCCWOP, RightCWOP, RightCCWOP,]
+
+# Winning state in a normal array format
+WINNING_STATE = [[[[0, 0], [0, 0]]], [[[1, 1], [1, 1]]], [[[2, 2], [2, 2]]], [[[3, 3], [3, 3]]], [[[4, 4], [4, 4]]],
+                 [[[5, 5], [5, 5]]]]
+
+# Gives each move equal probability
+P_normal = .083
+
+
+def T(s, a, sp):
+    '''
+    Calculate the transition probability for going from state s to state sp after taking
+    action a.
+    '''
+    if s == WINNING_STATE: return 0
+    if sp == WINNING_STATE: return 1
+    return P_normal
+
+
+
+
+def R(s, a, sp):
+    'Returns the reward associated with transitioning from s to sp via action a.'
+    if sp == WINNING_STATE: return 1000
+    facedict = {"U":0, "D":1, "F":2, "B":3, "R":4, "L":5}
+    facepointdict = {"U":0, "D":0, "F":2, "B":0, "R":0, "L":0}
+    for key, value in facedict.items():
+        count = 0
+        point = 0
+        for layer in range(len(sp[value])):
+            for cube in range(sp[value][layer]):
+                if sp[value][layer][cube] == value:
+                    count += 1
+        point = math.pow(2, count)
+        facepointdict.get(key, point)
+
+    totalRewards = 0
+    for key in facepointdict.items():
+        totalRewards += facepointdict.get(key)
+    return totalRewards
+
+
+
 if __name__ == "__main__":
     c = Cube()
-    c.move("U", 0, -1)
     print(c.flatBoard)
+    print(c.result)
+
+    c.moveCube("U", 0, -1)
+    # print(c.flatBoard)
+
 
 
